cf5858a8814b66af9110bffd9c0bce075a7af712
sunami09 <sunamidasgupta@gmail.com>
2024-12-03 17:28:06 -0800

    OCR Android Issue Fixed

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  await dotenv.load(fileName: '.env'); 
+  await dotenv.load(fileName: 'assets/.env'); 
   runApp(const MyApp());
 }
 
